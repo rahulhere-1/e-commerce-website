1274a020d378f12a1297c1edb04a9f295416cd3c
rahulhere-1 <rmendigeri4@gmail.com>
2025-02-12 19:39:15 +0530

    added more dao calls to category service

--- src/main/java/com/ecommerce/shopping_cart/repository/CategoryRepository.java
+++ src/main/java/com/ecommerce/shopping_cart/repository/CategoryRepository.java
@@ -1,11 +1,18 @@
 package com.ecommerce.shopping_cart.repository;
 
 import com.ecommerce.shopping_cart.model.Category;
+import com.ecommerce.shopping_cart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
 
     public Category findCategoryByName(String name);
+
+    @Query(" Select productList FROM Category WHERE id = ?1 ")
+    public List<Product> findProductListByCategoryId(Long id);
 
 
 }
--- src/main/java/com/ecommerce/shopping_cart/service/category/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/shopping_cart/service/category/CategoryServiceImpl.java
@@ -1,4 +1,17 @@
 package com.ecommerce.shopping_cart.service.category;
 
+import com.ecommerce.shopping_cart.model.Product;
+import com.ecommerce.shopping_cart.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Product> getProductsByCategoryId(Long id){
+        return categoryRepository.findProductListByCategoryId(id);
+    }
 }
